278d56fdd69ceddd81bee3e09bb0f8d10ed2349c
xyzuan <jodyyuan@xyzuan.com>
2026-04-04 15:46:00 +0000

    props: remove show_pixel_tos

--- configs/props.mk
+++ configs/props.mk
@@ -25,8 +25,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
     setupwizard.theme=glif_v3_light \
     setupwizard.feature.baseline_setupwizard_enabled=true \
     setupwizard.feature.skip_button_use_mobile_data.carrier1839=true \
-    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
-    setupwizard.feature.show_pixel_tos=false
+    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false
 
 # OPA configuration
 PRODUCT_PRODUCT_PROPERTIES += \
